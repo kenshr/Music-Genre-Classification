f786ce6f3da5185d0da8fc3f8ef2ab6d8caaf2ba
kenshr <kenshrivastava@gmail.com>
2021-04-26 20:52:06 -0700

    finalizing cleaned pipeline

--- src/audio_file_mgmt.py
+++ src/audio_file_mgmt.py
@@ -1,5 +1,7 @@
+import numpy as np
+import pandas as pd
 import os
-
+from tqdm import tqdm
 from pydub import AudioSegment
 import librosa
 
@@ -13,6 +15,73 @@ class AudioManagement:
     self.metadata_path = metadata_path
     self.split_dir_name = split_dir_name
     self.split = False
+
+
+  def make_spectrogram(self, track_id):
+    """
+    Takes a track_id, identifies the corresponding mp3's
+    file path, and returns a spectrogram of the mp3.
+
+    Parameters:
+    -----------
+    track_id (int):
+
+    Returns:
+    --------
+
+    """
+    # Format track id to match filename
+    tid = '{:06d}'.format(track_id)
+    # Create file path to mp3
+    path = os.path.join(self.audio_dir, tid[:3], tid + '.mp3')
+    # Generate spectrogram from mp3
+    y, sr = librosa.load(path)
+    spect = librosa.feature.melspectrogram(y=y, sr=sr)
+    spect = librosa.power_to_db(spect, ref=np.max)
+    return spect.T
+
+  def create_feature_label_data(self, split, destination='../data/'):
+    genres = []
+    X = np.empty((0, 640, 128))
+    count = 0
+
+    # Read in metadata
+    tracks = pd.read_csv(self.metadata_path, index_col=0, header=[0,1])
+    # Check for incorrect splits
+    if len(tracks[tracks[('set', 'split')] == split]) not in [800, 6400]:
+      return 'Please provide a valid split: train, validation, or test'
+    # Sort for small dataset and split specified in argument
+    df = tracks[(tracks[('set', 'subset')]=='small') & (tracks[('set', 'split') == split])]
+
+    genre_encoding = {'Electronic':0, 'Experimental':1, 'Folk':2, 'Hip-Hop':3,
+                      'Instrumental':4, 'International':5, 'Pop':6, 'Rock':7}
+
+    for index, row in tqdm(df.iterrows()):
+      try:
+        ct += 1
+        track_id = int(row['track_id'])
+        genre = str(row[('track', 'genre_top')])
+        spect = make_spectogram(track_id)
+        # Normalize for small shape differences from time discrepancies
+        spect = spect[:640, :]
+        X = np.append(X, [spect], axis=0)
+        genres.append(genre_encoding[genre])
+      except:
+        print("Couldn't process: ", ct)
+        continue
+    y = np.array(genres)
+
+    X = librosa.core.db_to_power(X)
+    X = np.log(X)
+
+    # shuffles data for model learning performance
+    shuffle = np.random.permutation(len(X))
+    X = X[shuffle]
+    y = y[shuffle]
+
+    np.savez(destination + split + '_data', X, y)
+    print(f'{split} data stored at {destination}{split}_data')
+
 
   def duplicate_filestructure(self):
     """
@@ -78,56 +147,9 @@ class AudioManagement:
     self.split = True
 
 
-  def make_spectrogram(self, track_id):
-    """
-    Takes a track_id, identifies the corresponding mp3's
-    file path, and returns a spectrogram of the mp3.
-
-    Parameters:
-    -----------
-    track_id (int):
-
-    Returns:
-    --------
-
-    """
-    # Format track id to match filename
-    tid = '{:06d}'.format(track_id)
-    # Create file path to mp3
-    path = os.path.join(self.audio_dir, tid_str[:3], tid_str + '.mp3')
-    # Generate spectrogram from mp3
-    y, sr = librosa.load(path)
-    spect = librosa.feature.melspectrogram(y=y, sr=sr, n_fft=2048, hop_length=1024)
-    spect = librosa.power_to_db(spect, ref=np.max)
-    return spect.T
-
-
-  def create_array(df):
-    genres = []
-    X_spect = np.empty((0, 640, 128))
-    count = 0
-    #Code skips records in case of errors
-    for index, row in df.iterrows():
-      try:
-        count += 1
-        track_id = int(row['track_id'])
-        genre = str(row[('track', 'genre_top')])
-        spect = create_spectogram(track_id)
-        # Normalize for small shape differences
-        spect = spect[:640, :]
-        X_spect = np.append(X_spect, [spect], axis=0)
-        genres.append(dict_genres[genre])
-        # if count % 100 == 0:
-        #   print("Currently processing: ", count)
-      except:
-        print("Couldn't process: ", count)
-        continue
-    y_arr = np.array(genres)
-    return X_spect, y_arr
-
-  def array_
-
 
 if __name__=="__main__":
-    # am = AudioManagement()
-    pass
+    am = AudioManagement()
+    am.create_feature_label_data(split='train')
+    am.create_feature_label_data(split='validation')
+    am.create_feature_label_data(split='test')
